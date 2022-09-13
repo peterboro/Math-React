f445460b16820e646e793b12d59043dc04a1c028
Peter Njuguna <Ptahbn@gmail.com>
2022-09-13 20:44:18 +0300

    Add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
